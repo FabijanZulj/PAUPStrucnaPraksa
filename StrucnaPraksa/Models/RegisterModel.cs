0657bdb204331b9e40055c2ebda0eb864636a465
Fabijan Z <fabijan.zulj@gmail.com>
2020-06-20 23:41:06 +0200

    Validation fix

--- StrucnaPraksa/Models/RegisterModel.cs
+++ StrucnaPraksa/Models/RegisterModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,13 @@ namespace StrucnaPraksa.Models
 {
     public class RegisterModel
     {
+        [MinLength(5, ErrorMessage = "Korisnicko ime se mora sastojati od vise od 5 znakova")]
         public string Username { get; set; }
+        [MinLength(5, ErrorMessage = "Lozinka se mora sastojati od vise od 5 znakova")]
         public string Password { get; set; }
+        [Required]
         public string Ime { get; set; }
+        [Required]
         public string Prezime { get; set; }
     }
 }
